73fc169753dfc3e648232e5ba971ee075939b08c
Drago2707 <54005875+Drago2707@users.noreply.github.com>
2023-01-18 16:14:20 -0500

    Create opp5 part 1
    working with the __dict__ magic word

---
+++ Actually Code/OOP Tutorial/opp5.py
@@ -0,0 +1,31 @@
+class Item:
+    pay_rate = 0.8  # Discount Class Atribute
+
+    def __init__(self, name: str, price: float or int, quantity: int):
+
+        # Run validations to the recived arguments
+        assert type(name) is str, f"name should be a string, got {type(name)}"
+        assert price >= 0 and (type(price) is float or type(
+            price) is int), f"price should be a float/int greater or equal than 0, got {price} ({type(price)})"
+        assert quantity >= 0 and type(
+            quantity) is int, f"quantity should be an integer greater or equal than 0, got {quantity} ({type(quantity)})"
+
+        # Assing to self object
+        self.name = name
+        self.price = price
+        self.quantity = 1
+
+        # Check if its necesary to change the self.quantity value
+        if not quantity == 1:
+            self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+
+item1 = Item("Phone", 100, 5)
+item2 = Item("Laptop", 1000, 3)
+
+print(Item.__dict__) #All the attibutes for the Class level
+print(item1.__dict__) # All the attributes for the instance level
+
