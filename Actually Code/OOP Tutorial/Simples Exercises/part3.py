c5c45a6ffee51a058f87e0e7ad6733fcdf80a064
Drago2707 <54005875+Drago2707@users.noreply.github.com>
2023-01-19 10:58:37 -0500

    Mini Proyect Update 1
    Added init,repr, func_confirmation, factory_restart

--- Actually Code/OOP Tutorial/Simples Exercises/part3.py
+++ Actually Code/OOP Tutorial/Simples Exercises/part3.py
@@ -6,9 +6,10 @@ import os
 
 
 class Library:
+    all_books = []
+    books = 0
+    library_path = os.path.join(os.getcwd(), "Books")
     try:
-        library_path = os.path.join(os.getcwd(), "Books")
-        validation = True
         if os.path.exists(library_path):
             pass
         else:
@@ -17,14 +18,42 @@ class Library:
             os.rmdir(library_path)
             os.mkdir(library_path)
             with open(os.path.join(library_path, "Validation.txt"), "w") as files:
+                files.write("program_open, if you delete me the program\n")
                 files.write(
-                    "new_program_open, if you delete me the program will start from 0 and all of the files will going to be deleted")
-                validation = True
+                    "will start from 0 and all of the files will going to be deleted")
         else:
-            validation = False
+            files = os.listdir(library_path)
+            if len(files) != 1:
+                books = len(files)-1
     except PermissionError as P:
         print(P)
         raise SystemExit
     except FileNotFoundError as F:
         print(F)
         raise SystemExit
+
+    def __init__(self, name: str, day: int, month: str) -> None:
+        self.name = name
+        self.day = day
+        self.month = month
+        self.number = Library.books + 1
+        Library.books += 1
+        Library.all_books.append(self)
+
+    def __repr__(self) -> str:
+        return f"The book {self.name}, are the book #({self.number}),\n Added in the day {self.day} of {self.month},"
+
+    def func_confirmation()->str:
+        while True:
+            confirmation=input("Are you sure? All of the files will going to be deleted. Y/N: ")
+            if confirmation.upper in ["Y","N"]:
+                break
+        return confirmation
+        
+    def factory_restart():
+        factory_confirmation=Library.func_confirmation()
+        if factory_confirmation =="Y":
+            os.rmdir(Library.library_path)
+            return
+        if factory_confirmation == "N":
+            return
