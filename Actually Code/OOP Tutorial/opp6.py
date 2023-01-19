c0e369d9ef1cf14a81ff3085c5ea77e9b90d04a5
Drago2707 <54005875+Drago2707@users.noreply.github.com>
2023-01-19 03:36:13 -0500

    Work with oop
    opp5: part2 of the __dict__ method
    opp6: working with the __repr__ method

--- Actually Code/OOP Tutorial/opp6.py
+++ Actually Code/OOP Tutorial/opp6.py
@@ -1,41 +1,32 @@
 class Item:
-    pay_rate = 0.8  # Discount Class Atribute
-
+    all = []
     def __init__(self, name: str, price: float or int, quantity: int):
-
         # Run validations to the recived arguments
         assert type(name) is str, f"name should be a string, got {type(name)}"
         assert price >= 0 and (type(price) is float or type(
             price) is int), f"price should be a float/int greater or equal than 0, got {price} ({type(price)})"
         assert quantity >= 0 and type(
             quantity) is int, f"quantity should be an integer greater or equal than 0, got {quantity} ({type(quantity)})"
-
         # Assing to self object
         self.name = name
         self.price = price
         self.quantity = 1
-
-        # Check if its necesary to change the self.quantity value
-        if not quantity == 1:
+        if not quantity == 1:  # Check if its necesary to change the self.quantity value
             self.quantity = quantity
+        # Actions to execute
+        Item.all.append(self)
 
-    def calculate_total_price(self):
-        return self.price * self.quantity
-
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price},{self.quantity})"
 
 
 item1 = Item("Phone", 100, 5)
-print(item1.name, item1.price, item1.quantity)
-item1.apply_discount()
-print(item1.price)
-
 item2 = Item("Laptop", 1000, 3)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+item3 = Item("Calbe", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyborad", 75, 5)
 
+print(Item.all)
 
-# print(Item.__dict__) #All the attibutes for the Class level
-# print(item1.__dict__) # All the attributes for the instance level
+# for instance in Item.all:
+#     print(instance.name)
