c99e6213be110a6eda9cf6c45b6f97196a76b1c3
Drago2707 <54005875+Drago2707@users.noreply.github.com>
2023-01-19 05:42:15 -0500

    Mini Proyect
    Creating a library part1
    
    separated in parts because github cares more about commits.

--- Actually Code/OOP Tutorial/Simples Exercises/part3.py
+++ Actually Code/OOP Tutorial/Simples Exercises/part3.py
@@ -1,5 +1,13 @@
 """Create a Library class that has a list of books and methods to add and remove books,
 search for a book by title, and display all books currently in the library.
-
 """
+
+import os
+
+class Library:
+    library_path=os.getcwd()
+    if os.path.exists(os.path.join(library_path,"Books")):
+        pass
+    else:
+        os.mkdir(os.path.join(library_path,"Books"))
 
