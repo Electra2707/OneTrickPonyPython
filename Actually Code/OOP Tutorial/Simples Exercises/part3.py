17de6ff680008d159d2fdd0db719159032248686
Drago2707 <54005875+Drago2707@users.noreply.github.com>
2023-01-19 09:34:21 -0500

    Mini proyect update
    Its incomplete

--- Actually Code/OOP Tutorial/Simples Exercises/part3.py
+++ Actually Code/OOP Tutorial/Simples Exercises/part3.py
@@ -4,10 +4,29 @@ search for a book by title, and display all books currently in the library.
 
 import os
 
-class Library:
-    library_path=os.getcwd()
-    if os.path.exists(os.path.join(library_path,"Books")):
-        pass
-    else:
-        os.mkdir(os.path.join(library_path,"Books"))
 
+class Library:
+    try:
+        library_path = os.getcwd()
+        validation = False
+        if os.path.exists(os.path.join(library_path, "Books")):
+            library_path = os.path.join(library_path, "Books")
+        else:
+            os.mkdir(os.path.join(library_path, "Books"))
+
+        with open(os.path.join(library_path, "Validation.txt"), "w") as files:
+
+            file = files.read()
+            if file.startswith("New"):
+                validation = True
+            else:
+                files.writelines(1, "Old")
+                validation = False
+            files.write("F")
+            validation = True
+    except PermissionError as P:
+        print(P)
+        raise SystemExit
+    except FileNotFoundError as F:
+        print(F)
+        raise SystemExit
