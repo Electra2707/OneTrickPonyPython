de8c315da8084e157e69e0d7eedb26b0294bce0d
Drago2707 <dragon27070@gmail.com>
2023-01-24 00:35:44 -0500

    opps

--- Actually Code/OOP Tutorial/New Information/item.py
+++ Actually Code/OOP Tutorial/New Information/item.py
@@ -3,19 +3,24 @@ import csv
 
 class Item:
     all = []
+    PAY_RATE = 0.8
 
     def __init__(self, name: str, price: float = 0, quantity: int = 0):
         # Assign to self object
+
         self.__name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
         Item.all.append(self)
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}',{self.price},{self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}',{self.__price},{self.quantity})"
 
     def calculate_total_price(self):
-        return self.price * self.quantity
+        return self.__price * self.quantity
+
+    def apply_discount(self):
+        self.__price = self.__price * self.PAY_RATE
 
     @classmethod
     def instantiate_from_csv(cls):
@@ -44,7 +49,13 @@ class Item:
         print("You are trying to get name")
         return self.__name
 
+    @property
+    def price(self):
+        return self.price
+
     @name.setter
     def name(self, value):
         print("You are trying to change the name")
+        if len(value) > 10:
+            raise Exception("The name is too long!")
         self.__name = value
