75ee4ce589032dc771e23e26cc08aadb5ffbc3db
Drago2707 <dragon27070@gmail.com>
2023-01-27 13:01:37 -0500

    Create Pract6.py

---
+++ Actually Code/CodeWars/Pract6.py
@@ -0,0 +1,17 @@
+"""Move the first letter of each word to the end of it, then add
+"ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+"""
+
+def pig_it(text: str) -> str:
+    list_of_words=text.split()
+    for word in list_of_words:
+        short_word=word[1:]
+        "".join()
+
+print(pig_it('Pig latin is cool'))#,'igPay atinlay siay oolcay')
+print(pig_it('This is my string'))#,'hisTay siay ymay tringsay')
+
