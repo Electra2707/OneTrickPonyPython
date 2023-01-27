93bfa31b4b6a4156f25b6972e83f0b603bf1edb3
Drago2707 <dragon27070@gmail.com>
2023-01-27 13:29:12 -0500

    finall

--- Actually Code/CodeWars/Pract6.py
+++ Actually Code/CodeWars/Pract6.py
@@ -6,14 +6,28 @@ pig_it('Pig latin is cool') # igPay atinlay siay oolcay
 pig_it('Hello world !')     # elloHay orldway !
 """
 
+
 def pig_it(text: str) -> str:
-    list_of_words=text.split()
-    new_word=[]
+    punctuation = ["!", "?", ".", ","]
+    list_of_words = text.split()
+    new_word = []
     for word in list_of_words:
-        word=word[1:]+word[:1]+"ay"
-        new_word.append(word)
+        if word in punctuation:
+            new_word.append(word)
+        else:
+            word = word[1:]+word[:1]+"ay"
+            new_word.append(word)
     return " ".join(new_word)
 
-print(pig_it('Pig latin is cool'))#,'igPay atinlay siay oolcay')
-print(pig_it('This is my string'))#,'hisTay siay ymay tringsay')
 
+def pig_it(text):
+    lst = text.split()
+    return ' '.join([word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
+
+
+def pig_it(text):
+    return " ".join(x[1:] + x[0] + "ay" if x.isalnum() else x for x in text.split())
+
+
+print(pig_it('O tempora o mores !'))  # ,'igPay atinlay siay oolcay')
+print(pig_it('This is my string'))  # ,'hisTay siay ymay tringsay')
