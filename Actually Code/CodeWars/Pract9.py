957d1d0ff667b76743650aac8879131e5f7407aa
Drago2707 <dragon27070@gmail.com>
2023-01-28 21:01:41 -0500

    other options
    jesus crist how bad it's my code

--- Actually Code/CodeWars/Pract9.py
+++ Actually Code/CodeWars/Pract9.py
@@ -30,5 +30,16 @@ def find_missing_letter(chars: list[str]) -> str:
     return missing_char[0].upper() if upper_case else missing_char[0]
 
 
+def find_missing_letter(chars):
+    n = 0
+    while ord(chars[n]) == ord(chars[n+1]) - 1:
+        n += 1
+    return chr(1+ord(chars[n]))
+
+
+def find_missing_letter(c):
+    return next(chr(ord(c[i])+1) for i in range(len(c)-1) if ord(c[i])+1 != ord(c[i+1]))
+
+
 print(find_missing_letter(['a', 'b', 'c', 'd', 'f']))  # , 'e')
 print(find_missing_letter(['O', 'Q', 'R', 'S']))  # , 'P')
