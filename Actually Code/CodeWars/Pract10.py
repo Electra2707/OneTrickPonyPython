ad5b0df5e6ce10fa7510e7a27fb156555de9bd28
Drago2707 <dragon27070@gmail.com>
2023-01-29 23:06:21 -0500

    yes

--- Actually Code/CodeWars/Pract10.py
+++ Actually Code/CodeWars/Pract10.py
@@ -190,6 +190,30 @@ def format_duration(seconds: int) -> str:
     return final_formatted_time + last_part
 
 
+
+times = [("year", 365 * 24 * 60 * 60), 
+         ("day", 24 * 60 * 60),
+         ("hour", 60 * 60),
+         ("minute", 60),
+         ("second", 1)]
+
+def format_duration(seconds):
+
+    if not seconds:
+        return "now"
+
+    chunks = []
+    for name, secs in times:
+        qty = seconds // secs
+        if qty:
+            if qty > 1:
+                name += "s"
+            chunks.append(str(qty) + " " + name)
+
+        seconds = seconds % secs
+
+    return ', '.join(chunks[:-1]) + ' and ' + chunks[-1] if len(chunks) > 1 else chunks[0]
+
 print(format_duration(1))  # Expected output: "1 second"
 print(format_duration(60))  # Expected output: "1 minute"
 print(format_duration(3600))  # Expected output: "1 hour"
