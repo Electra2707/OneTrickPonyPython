e585edd22776187a718d8cba724fe7a0c06b79a7
Drago2707 <dragon27070@gmail.com>
2023-01-31 13:25:06 -0500

    testing_part2

--- Actually Code/CodeWars/Pract11.py
+++ Actually Code/CodeWars/Pract11.py
@@ -32,6 +32,23 @@ def sum_pairs(ints: list[int], s: int):
 
 
 
+l1 = [1, 4, 8, 7, 3, 15]
+l2 = [1, -2, 3, 0, -6, 1]
+l3 = [20, -13, 40]
+l4 = [1, 2, 3, 4, 1, 0]
+l5 = [10, 5, 2, 3, 7, 5]
+l6 = [4, -2, 3, 3, 4]
+l7 = [0, 2, 0]
+l8 = [5, 9, 13, -3]
+l9 = [1] * 10000000
+l9[len(l9) // 2 - 1] = 6
+l9[len(l9) // 2] = 7
+l9[len(l9) - 2] = 8
+l9[len(l9) - 1] = -3
+l9[0] = 13
+l9[1] = 3
+
+
 # , [1, 7], "Basic: %s should return [1, 7] for sum = 8" % l1)
 print(1, sum_pairs(l1, 8))
 # , [0, -6], "Negatives: %s should return [0, -6] for sum = -6" % l2)
