da05b549586a8d3c8c366516e6bff3180d6e3d57
Drago2707 <dragon27070@gmail.com>
2023-02-01 12:34:54 -0500

    core

--- Actually Code/CodeWars/Pract12.py
+++ Actually Code/CodeWars/Pract12.py
@@ -4,3 +4,16 @@ corresponding length of nested arrays as the first array.
 """
 
 
+def calculate_len(argument: list):
+    equals_original = []
+    for element in argument:
+        if isinstance(element, int):
+            equals_original.append(-1)
+        elif isinstance(element, list):
+            if all(isinstance(item, list) for item in element):
+                equals_original.append(None)
+            else:
+                equals_original.append(len(element))
+    return equals_original
+
+
