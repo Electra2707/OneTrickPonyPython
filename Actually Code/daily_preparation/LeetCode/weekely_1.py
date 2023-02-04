a4e93bf1dd588cd1fa633c764f8940b8a3aa2046
Drago2707 <dragon27070@gmail.com>
2023-02-04 18:38:31 -0500

    advance

--- Actually Code/daily_preparation/LeetCode/weekely_1.py
+++ Actually Code/daily_preparation/LeetCode/weekely_1.py
@@ -3,4 +3,12 @@
 In other words, return true if one of s1's permutations is the substring of s2.
 """
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    def generate_lists(string:str) -> list:
+        pass
+    def checkInclusion(self, s: str, s2: str) -> bool:
+        items_in_s2 = Solution.generate_lists(s1)
+
+if __name__=="__main__":
+    s1=input()
+    s2=input()
+    print(Solution.checkInclusion(s1,s2))
