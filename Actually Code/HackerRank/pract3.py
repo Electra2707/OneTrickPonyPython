00f848df13adf9abedb07605f45aaf60fbf2ec75
Drago2707 <dragon27070@gmail.com>
2023-02-03 21:26:12 -0500

    advance

--- Actually Code/HackerRank/pract3.py
+++ Actually Code/HackerRank/pract3.py
@@ -6,4 +6,11 @@ if __name__ == '__main__':
     # s = input().upper()
     # assert 0 < len(s) and len(s) <= 10**6
     s = "BANANA"
+    VOWELS = set(["A", "E", "I", "O", "U"])
+    s_kevin = ""
+    s_stuart = ""
+    for i in s:
+        if i in VOWELS:
+            s_kevin = s.
+
     minion_game(s)
