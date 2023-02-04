6832d21bd24069ffcf6a6ddddac6b875eba303fe
Drago2707 <dragon27070@gmail.com>
2023-02-03 21:11:44 -0500

    advance

--- Actually Code/HackerRank/pract3.py
+++ Actually Code/HackerRank/pract3.py
@@ -3,5 +3,7 @@ def minion_game(string):
 
 
 if __name__ == '__main__':
-    s = input().upper()
+    # s = input().upper()
+    # assert 0 < len(s) and len(s) <= 10**6
+    s = "BANANA"
     minion_game(s)
