b5946479eb58c47a4c73a686f107e1d935ee7c42
Drago2707 <dragon27070@gmail.com>
2023-02-06 22:30:52 -0500

    eazy

--- Actually Code/daily_preparation/CodeWars/Pract13.py
+++ Actually Code/daily_preparation/CodeWars/Pract13.py
@@ -3,3 +3,15 @@ input string and remove duplicates, if present.
 
 Create as many "shufflings" as you can!
 """
+
+from itertools import permutations as per
+
+def permutations(s):
+    s = list(s)
+    return list(set(["".join(p) for p in per(s)]))
+
+
+print(permutations('a'))  # , ['a']);
+print(permutations('ab'))  # , ['ab', 'ba'])
+# , ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'])
+print(permutations('aabb'))
