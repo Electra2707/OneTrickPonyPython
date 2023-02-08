92892366bbf2a325219179f06b91971d1e5e07be
Drago2707 <dragon27070@gmail.com>
2023-02-07 20:48:21 -0500

    progress

--- Actually Code/daily_preparation/LeetCode/pract3.py
+++ Actually Code/daily_preparation/LeetCode/pract3.py
@@ -5,3 +5,21 @@ An Anagram is a word or phrase formed by rearranging the letters of a
 different word or phrase, typically using all the original letters exactly once.
 """
 
+
+class Solution:
+    def findAnagrams(self, long_string: str, find_string: str) -> list[int]:
+        counter = []
+        find_length = len(find_string)
+        try:
+            for i in range(len(long_string)):
+                anagram = long_string[:i] + long_string[:find_length]
+                if find_string == anagram:
+                    counter.append(i)
+        except IndexError:
+            pass
+        return counter
+
+
+s = "cbaebabacd"
+p = "abc"
+print(Solution().findAnagrams(s, p))
