def6f1d602c47e76ab998c3d99eab99c36393f3b
Drago2707 <dragon27070@gmail.com>
2023-02-07 22:21:47 -0500

    OMG IT' WORKS

--- Actually Code/daily_preparation/LeetCode/pract4.py
+++ Actually Code/daily_preparation/LeetCode/pract4.py
@@ -1,18 +1,24 @@
 class Solution:
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
     def romanToInt(self, s: str) -> int:
         conversion = 0
-        values = reversed([roman_dict[x] for x in s])
+        values = [Solution.roman_dict[x] for x in s]
+        new_values = [-1] + values
+        for num1, num2 in zip(values, new_values):
+            if num1>num2:
+                conversion = conversion + num1
+            elif num1<num2:
+                conversion = conversion - num1
+        return conversion
 
-        return sum(values)
 
-
-roman_dict = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-}
-print(Solution().romanToInt("III"))
+print(Solution().romanToInt("MCMXCIV"))
