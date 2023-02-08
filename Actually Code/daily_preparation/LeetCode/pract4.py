df70d35d3ccf332389b939f3a5f354eb406207af
Drago2707 <dragon27070@gmail.com>
2023-02-07 22:02:10 -0500

    progress

--- Actually Code/daily_preparation/LeetCode/pract4.py
+++ Actually Code/daily_preparation/LeetCode/pract4.py
@@ -2,8 +2,8 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         conversion = 0
         values = reversed([roman_dict[x] for x in s])
-        
-        return conversion
+
+        return sum(values)
 
 
 roman_dict = {
@@ -13,6 +13,6 @@ roman_dict = {
     "L": 50,
     "C": 100,
     "D": 500,
-    "M": 100
+    "M": 1000
 }
-print(Solution().romanToInt("LVIII"))
+print(Solution().romanToInt("III"))
