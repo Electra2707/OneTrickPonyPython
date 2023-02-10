0144f6f3bbac40bc2027a6ac32bdc4e370f1d219
Drago2707 <dragon27070@gmail.com>
2023-02-10 11:15:35 -0500

    don't understant shit

--- Actually Code/daily_preparation/LeetCode/pract5.py
+++ Actually Code/daily_preparation/LeetCode/pract5.py
@@ -16,9 +16,29 @@ grid[i][j] is 0 or 1
 """
 
 
+from collections import deque
+
+
 class Solution:
     def maxDistance(self, grid: list[list[int]]) -> int:
         max_dist = -1
+        queue = deque()
+        visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    queue.append((i, j, 0))
+                    visited.add((i, j))
+        if not queue or len(queue) == len(grid) * len(grid[0]):
+            return max_dist
+        while queue:
+            i,j, dist = queue.popleft()
+            max_dist = max(dist, dist)
+            for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and (x, y) not in visited:
+                    queue.append((x, y, dist + 1))
+                    visited.add((x, y))
+        return max_dist
         # for dimension in grid:
         #     counter = -1
         #     if counter != -1:
@@ -31,15 +51,12 @@ class Solution:
         #             counter += 1
         #             if counter > max_dist:
         #                 max_dist = counter
-                        
-        # visited = set()
-        # for i, dimension in enumerate(grid):
-        #     for j, element in enumerate(dimension):
-        #         vertex = 
 
         return max_dist
 
 
 print(Solution().maxDistance(
-    [[1,0,0],[0,0,0],[0,0,0]]
+    [[1, 0, 0],
+     [0, 0, 0],
+     [0, 0, 0]]
 ))
