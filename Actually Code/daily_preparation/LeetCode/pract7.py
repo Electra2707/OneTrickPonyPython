e982e43ee0518c79adf9af2887263a15dbbf322f
Drago2707 <dragon27070@gmail.com>
2023-02-11 22:05:54 -0500

    progress

--- Actually Code/daily_preparation/LeetCode/pract7.py
+++ Actually Code/daily_preparation/LeetCode/pract7.py
@@ -14,20 +14,34 @@ Constraints:
 s consists of parentheses only '()[]{}'.
 """
 
+
 class Solution:
     def isValid(self, string: str) -> bool:
-        length_string=len(string)
+        length_string = len(string)
         if length_string <= 1:
             return False
-        
-        for i,letter in enumerate(string):
+        for i in range(len(string)):
             if i == length_string:
                 break
-            print(i,letter)
+            parenthesis_1 = ord(string[i])
+            parenthesis_2 = ord(string[i+1])
+            if parenthesis_1 == 40:
+                if parenthesis_2 == 41:
+                    continue
+            elif parenthesis_1 == 91:
+                if parenthesis_2 == 93:
+                    continue
+            elif parenthesis_1 == 123:
+                if parenthesis_2 == 126:
+                    continue
+            else:
+                return False
+
         return True
 
-print(Solution().isValid("()")) #True
+
+print(Solution().isValid("()"))  # True
 print("------------------------------------")
-print(Solution().isValid("()[]{}")) #False
+print(Solution().isValid("()[]{}"))  # False
 print("------------------------------------")
-print(Solution().isValid("(]")) #false
+print(Solution().isValid("(]"))  # false
