d4439fd52a8b8f18acecd0523fe36ddd47ef3760
Drago2707 <dragon27070@gmail.com>
2023-02-13 00:28:36 -0500

    new exercise

--- Actually Code/daily_preparation/CodeWars/Linked list/linked_list_to_string.py
+++ Actually Code/daily_preparation/CodeWars/Linked list/linked_list_to_string.py
@@ -12,5 +12,10 @@ class Node():
         self.data = data
         self.next = next
 
-    def stringify(node):
-        pass
+def stringify(node):
+    pass
+
+
+stringify(Node(0, Node(1, Node(2, Node(3)))))#, '0 -> 1 -> 2 -> 3 -> None')#
+stringify(None)#, 'None'
+stringify(Node(0, Node(1, Node(4, Node(9, Node(16))))))#, '0 -> 1 -> 4 -> 9 -> 16 -> None')
