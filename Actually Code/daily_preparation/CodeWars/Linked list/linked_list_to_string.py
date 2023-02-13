7f7234add6edfdfadbf4214b831ad59bce33bdaf
Drago2707 <dragon27070@gmail.com>
2023-02-13 01:16:47 -0500

    functional

--- Actually Code/daily_preparation/CodeWars/Linked list/linked_list_to_string.py
+++ Actually Code/daily_preparation/CodeWars/Linked list/linked_list_to_string.py
@@ -10,20 +10,20 @@ class Node():
         self.data = data
         self.next = next
 
-# def stringify(node):
-#     if node is None:
-#         return "None"
-#     else:
-#         string = str(node.data) + "->"
-#         return string + stringify(node.next)
-
 def stringify(node):
-    result = []
-    while node:
-        result.append(str(node.data))
-        node = node.next
-    result.append("None")
-    return " -> ".join(result)
+    if node is None:
+        return "None"
+    else:
+        string = str(node.data) + " -> "
+        return string + stringify(node.next)
+
+# def stringify(node):
+#     result = []
+#     while node:
+#         result.append(str(node.data))
+#         node = node.next
+#     result.append("None")
+#     return " -> ".join(result)
 
 
 print(stringify(Node(0, Node(1, Node(2, Node(3))))))#, '0 -> 1 -> 2 -> 3 -> None')#
