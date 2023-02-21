7c6b041e9285ca0b315ea2bc01fae48f31d8774e
Drago2707 <dragon27070@gmail.com>
2023-02-20 22:22:51 -0500

    new_pract

---
+++ Actually Code/daily_preparation/CodeWars/Linked_list_practs/Iterative Reverse.py
@@ -0,0 +1,10 @@
+"""Iterative Reverse
+Write an iterative Reverse() function that reverses a linked list. 
+Ideally, Reverse() should only need to make one pass of the list.
+
+var list = 2 -> 1 -> 3 -> 6 -> 5 -> null
+reverse(list)
+list === 5 -> 6 -> 3 -> 1 -> 2 -> null
+"""
+
+
