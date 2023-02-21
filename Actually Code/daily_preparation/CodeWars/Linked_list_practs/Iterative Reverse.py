b571a17610e8b9f3533c7d9d9bd8eb56a859fc90
Drago2707 <dragon27070@gmail.com>
2023-02-20 22:40:55 -0500

    kind of try

--- Actually Code/daily_preparation/CodeWars/Linked_list_practs/Iterative Reverse.py
+++ Actually Code/daily_preparation/CodeWars/Linked_list_practs/Iterative Reverse.py
@@ -15,12 +15,59 @@ class Node(object):
 
 
 def reverse(head):
-    previous = None
+    head = node_to_list(head)
+    return list_to_node(list(reversed(head)))
+
+
+def node_to_list(head):
+    if head is None:
+        return []
+    result = []
+    while head:
+        result.append(head.data)
+        head = head.next
+    return result
+
+
+def list_to_node(lst: list):
+    if not lst:
+        return None
+    head = Node(lst[0])
     current = head
-    while current:
-        next = current.next  # save next node
-        current.next = previous  # reverse link
-        previous = current  # update previous node
-        current = next  # update current node
-    head = previous  # update head pointer
+    for element in lst[1:]:
+        current.next = Node(element)
+        current = current.next
     return head
+
+
+# def reverse(head):
+#     previous = None
+#     current = head
+#     while current:
+#         next = current.next  # save next node
+#         current.next = previous  # reverse link
+#         previous = current  # update previous node
+#         current = next  # update current node
+#     head = previous  # update head pointer
+#     return head
+def string_to_node(string: str):
+    if string == "None":
+        return None
+    elements = list(map(int, string.split(" -> ")[:-1]))
+    head = None
+    for i in reversed(range(len(elements))):
+        head = Node(elements[i], head)
+    return head
+
+
+def node_to_string(node):
+    result = []
+    while node:
+        result.append(str(node.data))
+        node = node.next
+    result.append("None")
+    return " -> ".join(result)
+
+
+testing = string_to_node("3 -> 1 -> None")
+print(node_to_string(reverse(testing)))
