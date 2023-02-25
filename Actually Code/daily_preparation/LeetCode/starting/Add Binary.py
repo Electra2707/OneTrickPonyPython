6cc5bed847687d50dbc34584be6a0a0650fc5b6e
Drago2707 <dragon27070@gmail.com>
2023-02-24 23:15:41 -0500

    other solution

--- Actually Code/daily_preparation/LeetCode/starting/Add Binary.py
+++ Actually Code/daily_preparation/LeetCode/starting/Add Binary.py
@@ -10,8 +10,33 @@ Example 2:
 Input: a = "1010", b = "1011"
 Output: "10101"
 """
+
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return bin(int(a, 2) + int(b, 2))[2:]
 
-print(Solution().addBinary("11","1"))
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        # Initialize the result string and the carry bit
+        result = ""
+        carry = 0
+        # Convert the strings to integers
+        a = int(a, 2)
+        b = int(b, 2)
+        # Loop until both numbers are zero or there is a carry bit
+        while a or b or carry:
+            # Add the least significant bits of both numbers and the carry bit
+            sum = (a & 1) + (b & 1) + carry
+            # Append the least significant bit of the sum to the result string
+            result = str(sum & 1) + result
+            # Update the carry bit and right shift both numbers by one bit
+            carry = sum >> 1
+            a >>= 1
+            b >>= 1
+        # Return the result string
+        return result
+
+
+print(Solution().addBinary("11", "1"))
