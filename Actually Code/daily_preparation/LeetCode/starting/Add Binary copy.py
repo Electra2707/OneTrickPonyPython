a1a1874adffd7ff785b3e8b177a54dd07280e409
Drago2707 <dragon27070@gmail.com>
2023-02-27 14:03:39 -0500

    debugged and functional

--- Actually Code/daily_preparation/LeetCode/starting/Add Binary copy.py
+++ Actually Code/daily_preparation/LeetCode/starting/Add Binary copy.py
@@ -26,25 +26,29 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if not x:
             return 0
+        elif x == 1:
+            return 1
         prev = 0
-        for i in range(x):
-            if (i * i) < x:
+        for i in range(1, x):
+            if (i * i) <= x:
                 prev = i
             else:
                 break
         return prev
-print(Solution().mySqrt(1))
-print(Solution().mySqrt(2))
-print(Solution().mySqrt(3))
-print(Solution().mySqrt(4))
-print(Solution().mySqrt(5))
-print(Solution().mySqrt(6))
-print(Solution().mySqrt(7))
-print(Solution().mySqrt(8))
-print(Solution().mySqrt(9))
-print(Solution().mySqrt(10))
-print(Solution().mySqrt(11))
-print(Solution().mySqrt(12))
-print(Solution().mySqrt(13))
-print(Solution().mySqrt(14))
-print(Solution().mySqrt(15))
+
+
+print(Solution().mySqrt(1), "1,1")
+print(Solution().mySqrt(2), "2")
+print(Solution().mySqrt(3), "3")
+print(Solution().mySqrt(4), "4,2")
+print(Solution().mySqrt(5), "5")
+print(Solution().mySqrt(6), "6")
+print(Solution().mySqrt(7), "7")
+print(Solution().mySqrt(8), "8")
+print(Solution().mySqrt(9), "9,3")
+print(Solution().mySqrt(10), "10")
+print(Solution().mySqrt(11), "11")
+print(Solution().mySqrt(12), "12")
+print(Solution().mySqrt(13), "13")
+print(Solution().mySqrt(14), "14")
+print(Solution().mySqrt(15), "15")
