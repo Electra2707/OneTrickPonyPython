4946e541a30eac966e46ec0f448a571508ab9dfb
Drago2707 <dragon27070@gmail.com>
2023-02-28 13:25:07 -0500

    curses windows

--- Actually Code/pylibs_preparation/curses/curses5.py
+++ Actually Code/pylibs_preparation/curses/curses5.py
@@ -2,18 +2,23 @@ import curses
 from curses import wrapper
 import time
 
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
     curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
     BLUE_AND_YELLOW = curses.color_pair(1)
     MANGENTA_AND_WHITE = curses.color_pair(2)
+
+    counter_win = curses.newwin(1, 20, 10, 10)
+    stdscr.addstr("Hello World")
+    stdscr.refresh()
     for i in range(101):
-        stdscr.clear()
+        counter_win.clear()
         if i % 2 == 0:
-            stdscr.addstr(f"counter: {i}", MANGENTA_AND_WHITE)
+            counter_win.addstr(f"counter: {i}", MANGENTA_AND_WHITE)
         else:
-            stdscr.addstr(f"counter: {i}", BLUE_AND_YELLOW)
-        stdscr.refresh()
+            counter_win.addstr(f"counter: {i}", BLUE_AND_YELLOW)
+        counter_win.refresh()
         time.sleep(0.1)
     stdscr.getch()
 
