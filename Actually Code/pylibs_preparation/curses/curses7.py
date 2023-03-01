94bf4143bc195ac1502f2d75a738613449d28e3e
Drago2707 <dragon27070@gmail.com>
2023-02-28 20:40:41 -0500

    stuff

---
+++ Actually Code/pylibs_preparation/curses/curses7.py
@@ -0,0 +1,30 @@
+import curses
+from curses import wrapper
+from time import sleep
+
+def main(stdscr):
+    maxy, maxx = stdscr.getmaxyx()  # get maximum coordinates
+    maxy, maxx = maxy - 1, maxx-1
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+    curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
+    BLUE_AND_YELLOW = curses.color_pair(1)
+    MANGENTA_AND_WHITE = curses.color_pair(2)
+    pad = curses.newpad(100, 100)
+    stdscr.refresh()
+    for i in range(100):
+        for j in range(26):
+            char = chr(65 + j)
+            if j % 2 == 0:
+                pad.addstr(char)
+            else:
+                pad.addstr(char)
+    for i in range(10):
+        stdscr.clear()
+        stdscr.refresh()
+        pad.refresh(0, 0, 0, i, maxy-1, round((maxx*0.75))+i)
+        sleep(0.1)
+
+    stdscr.getch()
+
+
+wrapper(main)
