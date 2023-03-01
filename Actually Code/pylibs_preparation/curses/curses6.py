cb32400442c8f0cf0de4983a347551208294a7d9
Drago2707 <dragon27070@gmail.com>
2023-02-28 19:34:03 -0500

    omg

--- Actually Code/pylibs_preparation/curses/curses6.py
+++ Actually Code/pylibs_preparation/curses/curses6.py
@@ -1,10 +1,10 @@
 import curses
 from curses import wrapper
-import time
 
 
 def main(stdscr):
-    maxy, maxx = stdscr.getmaxyx() # get maximum coordinates
+    maxy, maxx = stdscr.getmaxyx()  # get maximum coordinates
+    maxy, maxx = maxy - 1, maxx-1
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
     curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
     BLUE_AND_YELLOW = curses.color_pair(1)
@@ -13,12 +13,14 @@ def main(stdscr):
     stdscr.refresh()
     for i in range(100):
         for j in range(26):
-            char = chr(67 + j)
+            char = chr(65 + j)
             if j % 2 == 0:
                 pad.addstr(char, BLUE_AND_YELLOW)
             else:
                 pad.addstr(char, MANGENTA_AND_WHITE)
-    pad.refresh(0,0,0,0,0,0)
+    print(round((maxx/0.25)))
+    pad.refresh(0, 0, maxy//2, maxx//4, maxy, round((maxx*0.75)))
+
     stdscr.getch()
 
 
