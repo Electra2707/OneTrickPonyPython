61efc791bb1abd1a9d0680e3771c81ff8a40c601
Drago2707 <dragon27070@gmail.com>
2023-06-16 18:08:48 -0500

    Nothing

--- Actually Code/tutorial/More Advanced Exercices/Simple calculator.py
+++
@@ -1,7 +0,0 @@
-"""Write a program that implements a simple calculator.
-The program should prompt the user for two numbers and an operator (e.g. +, -, *, /),
-and then perform the corresponding operation on the numbers.
-    """
-
-class Calculator:
-    pass
